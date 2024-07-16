1e09ad5149b4e75f39da8710ab32314b505c40bf
Alvin Neal <87945028+CodenameBunny@users.noreply.github.com>
2024-07-16 23:22:58 +0530

    Create Document.js

---
+++ src/Options/Documents/Document.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Document(props){
+
+    const tc = () => {
+        props.actions.tc()
+    }
+    const studycert = () => {
+        props.actions.studycert()
+    }
+    const passcert = () => {
+        props.actions.passcert()
+    }
+
+    return [
+        <div>
+            <button className="button-0" onClick={() => tc()}>TC</button>
+            <button className="button-0" onClick={() => studycert()}>Study Certificate</button>
+            <button className="button-0" onClick={() => passcert()}>Pass Certificate</button>
+        </div>
+    ]
+}
