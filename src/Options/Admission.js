cf1095bfb3c344cfa772ebde32bbda6019a122d2
Alvin Neal <87945028+CodenameBunny@users.noreply.github.com>
2024-07-16 23:21:53 +0530

    Create Admission.js

---
+++ src/Options/Admission.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Admission(){
+    return [
+        <div>
+            <button className="button-1"><a href="https://sjbhs.edu.in/Admission_to_LKG.php">LKG</a></button>
+            <button className="button-0"><a href="https://sjbhs.edu.in/applying_for_std11.php">ISC</a></button>
+        </div>
+    ]
+}
