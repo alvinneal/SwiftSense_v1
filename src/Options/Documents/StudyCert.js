566b55107df8939275db4e9e343bede74997b6a9
Alvin Neal <87945028+CodenameBunny@users.noreply.github.com>
2024-07-16 23:23:13 +0530

    Add files via upload

---
+++ src/Options/Documents/StudyCert.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function StudyCert(props){
+
+    const aadharsc = () => {
+        props.actions.aadharsc()
+    }
+    const castesc = () => {
+        props.actions.castesc()
+    }
+    const entrancesc = () => {
+        props.actions.entrancesc()
+    }
+
+    return [
+        <div>
+            <button className="button-1" onClick={() => aadharsc()}>Aadhar Card</button>
+            <button className="button-0" onClick={() => castesc()}>Caste</button>
+            <button className="button-0" onClick={() => entrancesc()}>Entrance</button>
+        </div>
+    ]
+}
