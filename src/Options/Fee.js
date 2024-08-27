6203c1ff42f12a72fc2217651c4a71f15bb1fa80
Alvin Neal <87945028+CodenameBunny@users.noreply.github.com>
2024-08-28 01:14:20 +0530

    Fee.js

--- src/Options/Fee.js
+++ src/Options/Fee.js
@@ -19,11 +19,11 @@ export default function Fee(props){
     }
     return [
         <div>
-            <button className="button-0" onClick={() => schoolfee()}>School</button>
+            <button className="button-1" onClick={() => schoolfee()}>School</button>
             <button className="button-0" onClick={() => busfee()}>Bus</button>
             <button className="button-0" onClick={() => frenchfee()}>French</button>
             <button className="button-0" onClick={() => clubfee()}>Clubs</button>
-            <button className="button-0" onClick={() => ptafee()}>PTA</button>
+            <button className="button-1" onClick={() => ptafee()}>PTA</button>
         </div>
     ]
 }
