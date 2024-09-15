8cebb60308e6cc8055fdfdcbb76c56451a51ca32
Alvin Neal <87945028+alvinneal@users.noreply.github.com>
2024-09-15 11:27:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,59 +8,62 @@ import Books from './Options/Books';
 import StudyCert from './Options/Documents/StudyCert';
 import Document from './Options/Documents/Document';
 import Other from './Options/Other';
+import Timing from './Options/Timing';
 
 const config = {
-    initialMessages: [createChatBotMessage(`Hi there I'm SwiftSense, Choose an Option for Details.`, {
-        widget: "button_0"
-    })],
-    botName: "SwiftSense",
-    customStyles: {
-      botMessageBox: {
-        backgroundColor: '#a238c7',
-      },
-      chatButton: {
-        backgroundColor: '#a238c7',
-      },
+  botName: "SwiftSense",
+  initialMessages: [createChatBotMessage(`Hi there I'm SwiftSense, Choose an Option for Details.`, {
+    widget: "button_0"
+  })],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: '#a238c7',
     },
-    customComponents: {
-        botAvatar: (props) => <Avatar {...props} />
+    chatButton: {
+      backgroundColor: '#a238c7',
     },
-
-
-    widgets: [
-        {
-            widgetName:  'button_0',
-            widgetFunc: (props) => <Button_0 {...props} />,
-        },
-        {
-            widgetName:  'admission',
-            widgetFunc: (props) => <Admission {...props} />,
-        },
-        {
-            widgetName:  'fee',
-            widgetFunc: (props) => <Fee {...props} />,
-        },
-        {
-            widgetName:  'uniform',
-            widgetFunc: (props) => <Uniform {...props} />,
-        },
-        {
-            widgetName:  'books',
-            widgetFunc: (props) => <Books {...props} />,
-        },
-        {
-            widgetName:  'document',
-            widgetFunc: (props) => <Document {...props} />,
-        },
-        {
-            widgetName:  'studycert',
-            widgetFunc: (props) => <StudyCert {...props} />,
-        },
-        {
-            widgetName:  'other',
-            widgetFunc: (props) => <Other {...props} />,
-        },
-    ]
-  };
+  },
+  customComponents: {
+    botAvatar: (props) => <Avatar {...props} />
+  },
+  widgets: [
+    {
+      widgetName: 'button_0',
+      widgetFunc: (props) => <Button_0 {...props} />,
+    },
+    {
+      widgetName: 'admission',
+      widgetFunc: (props) => <Admission {...props} />,
+    },
+    {
+      widgetName: 'fee',
+      widgetFunc: (props) => <Fee {...props} />,
+    },
+    {
+      widgetName: 'uniform',
+      widgetFunc: (props) => <Uniform {...props} />,
+    },
+    {
+      widgetName: 'books',
+      widgetFunc: (props) => <Books {...props} />,
+    },
+    {
+      widgetName: 'document',
+      widgetFunc: (props) => <Document {...props} />,
+    },
+    {
+      widgetName: 'studycert',
+      widgetFunc: (props) => <StudyCert {...props} />,
+    },
+    {
+      widgetName: 'timing',
+      widgetFunc: (props) => <Timing {...props} />,
+    },
+    {
+      widgetName: 'other',
+      widgetFunc: (props) => <Other {...props} />,
+    },
+  ]
+};
 
 export default config;
